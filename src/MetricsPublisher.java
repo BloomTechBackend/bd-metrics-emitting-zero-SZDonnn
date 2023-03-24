a89d8d5a73eac0314d1fb1d6a47868e2a3cf7a8f
SZ-Don <don.platan@talentamp.com>
2023-03-24 13:53:35 +0800

    Test passed.

--- src/MetricsPublisher.java
+++ src/MetricsPublisher.java
@@ -1,6 +1,8 @@
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.cloudwatch.AmazonCloudWatch;
 import com.amazonaws.services.cloudwatch.AmazonCloudWatchClientBuilder;
+import com.amazonaws.services.cloudwatch.model.Dimension;
+import com.amazonaws.services.cloudwatch.model.MetricDatum;
 import com.amazonaws.services.cloudwatch.model.PutMetricDataRequest;
 import com.amazonaws.services.cloudwatch.model.StandardUnit;
 
@@ -33,8 +35,19 @@ public class MetricsPublisher {
      */
     public PutMetricDataRequest buildMetricDataRequest(final String metricName, final double value,
                                                        final StandardUnit unit) {
-
         // TODO: implement
-        return null;
+        Dimension dimension = new Dimension()
+                .withName("ENVIRONMENT")
+                .withValue("PRODUCTION");
+
+        MetricDatum datum = new MetricDatum()
+                .withMetricName(metricName)
+                .withValue(value)
+                .withUnit(unit)
+                .withDimensions(dimension);
+
+        return new PutMetricDataRequest()
+                .withNamespace("EXAMPLE/ORDERS")
+                .withMetricData(datum);
     }
 }
